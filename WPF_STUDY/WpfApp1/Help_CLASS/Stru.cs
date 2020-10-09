87733f1d63a4dcad3fe06e9ac87b7581190585b8
812882781 <50092203+812882781@users.noreply.github.com>
2020-10-09 17:46:43 +0800

    Add files via upload

---
+++ WPF_STUDY/WpfApp1/Help_CLASS/Stru.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace WpfApp1.Help_CLASS
+{
+    /// <summary>
+    /// 存放通用的属性及结构
+    /// </summary>
+    class Stru
+    {
+        /// <summary>
+        /// 通用字段
+        /// </summary>
+        private string comm_str;
+
+        public string Comm_str { get { return "v"; } }
+
+
+    }
+}
